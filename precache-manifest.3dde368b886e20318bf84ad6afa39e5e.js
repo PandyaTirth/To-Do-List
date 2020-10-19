2a4ec40b8b9e5333f79ba0f5953f614cc63ef484
Admin <pandyatirth08@gmail.com>
2020-10-19 16:38:48 +0530

    Updates

---
+++ precache-manifest.3dde368b886e20318bf84ad6afa39e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a35f10674c129c5bce212eb4795d55",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "04a63af605129389b1c2",
+    "url": "/To-Do-List/static/css/main.e2a06210.chunk.css"
+  },
+  {
+    "revision": "0d8f4d465d2e86ab810f",
+    "url": "/To-Do-List/static/js/2.c085d153.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.c085d153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a63af605129389b1c2",
+    "url": "/To-Do-List/static/js/main.fafecc5c.chunk.js"
+  },
+  {
+    "revision": "3fc2ca48924706e73961",
+    "url": "/To-Do-List/static/js/runtime-main.ce724f51.js"
+  }
+]);
